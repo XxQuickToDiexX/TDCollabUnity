6f1f9e31dc30ff733e2a5c342d14c45b82d45a47
Unknown <obliviondrums@yahoo.com>
2016-11-07 13:27:30 -0800

    Changes to map, enemy pathing in, turret follows enemy

---
+++ TowerDefenseCollab/Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	GameObject pathing;
+
+	Transform targetPathNode;
+	int pathIndex = 0;
+
+	float speed = 5f;
+
+	public float health = 1f;
+
+	void Start () 
+	{
+		pathing = GameObject.Find("Path");
+	}
+
+	void GetNextPathNode()
+	{
+		if(pathIndex < pathing.transform.childCount)
+		{
+			targetPathNode = pathing.transform.GetChild(pathIndex);
+			pathIndex++;
+		}
+		else 
+		{
+			targetPathNode = null;
+			ReachedLastNode();
+		}
+	}
+
+	void Update () {
+		if(targetPathNode == null)
+		{
+			GetNextPathNode();
+			if(targetPathNode == null) 
+			{
+				ReachedLastNode();
+				return;
+			}
+		}
+
+		Vector3 direction = targetPathNode.position - this.transform.localPosition;
+
+		float distance = speed * Time.deltaTime;
+
+		if(direction.magnitude <= distance) {
+			// We reached the node
+			targetPathNode = null;
+		}
+		else {
+			// TODO: Consider ways to smooth this motion.
+
+			// Move towards node
+			transform.Translate( direction.normalized * distance, Space.World );
+
+			//Rotate to look at Node
+			Quaternion targetRotation = Quaternion.LookRotation( direction );
+
+			//Smooth the rotation
+			this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime*5);
+		}
+
+	}
+
+	void ReachedLastNode() {
+		Destroy(gameObject);
+	}
+}
---
+++ TowerDefenseCollab/Assets/Scripts/Tower.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour 
+{
+	Transform tower;
+
+	void Start () 
+	{
+		tower = this.transform; // Get the turret
+	}
+
+	void Update () 
+	{
+		Enemy[] enemies = GameObject.FindObjectsOfType<Enemy> (); // Array of all enemies
+
+		Enemy nearestEnemy = null;
+
+		float distance = Mathf.Infinity;
+
+		foreach (Enemy e in enemies)
+		{
+			float dist = Vector3.Distance (this.transform.position, e.transform.position);
+
+			if (nearestEnemy == null || dist < distance) {
+				nearestEnemy = e;
+				distance = dist;
+			} 
+			else if (nearestEnemy == null) 
+			{
+				Debug.Log ("No Enemies");
+				return;
+			}
+		}
+
+		if (nearestEnemy != null)
+		{
+			LookToEnemy(nearestEnemy);
+		}
+
+	}
+
+	void LookToEnemy(Enemy e)
+	{
+		//Make vector from enemy to tower
+		Vector3 direction = e.transform.position - this.transform.position;
+
+		//Rotate to look at the enemy
+		Quaternion lookRotation = Quaternion.LookRotation (direction);
+		tower.rotation = Quaternion.Euler (0, lookRotation.eulerAngles.y, 0);
+	}
+}
